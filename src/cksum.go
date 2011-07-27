c77653a4a24e0c4def852998e7aa94eabc58803b
Ian Gilham <ian.gilham@gmail.com>
2011-07-27 22:00:05 +0100

    made cksum take a file and made it more similar to the posix cksum tool

--- src/cksum.go
+++ src/cksum.go
@@ -5,49 +5,52 @@ package main
 import (
     "flag"
     "fmt"
-    "hash/crc64"
+    "hash"
+    "hash/crc32"
     "os"
 )
 
-// prints the checksum of each file argument
+// prints the checksum  and the size of the checksum in bytes of each file argument
 func main() {
     flag.Parse()
     if flag.NArg() == 0 {
-        fmt.Printf("no args\n")
+        if h := cksum(os.Stdin); h != nil {
+            fmt.Printf("%d %d\n", h.Sum32(), h.Size())
+        }
     } else {
         for i := 0; i < flag.NArg(); i++ {
-            cksum(flag.Arg(i))
+            if fd, err := os.Open(flag.Arg(i)); err != nil {
+                fmt.Printf("error reading file %s\n", flag.Arg(i))
+            } else {
+                if h := cksum(fd); h != nil {
+                    fmt.Printf("%d %d %s\n", h.Sum32(), h.Size(), fd.Name())
+                }
+            }
         }
     }
 }
 
-func cksum(s string) {
-    table := crc64.MakeTable(crc64.ISO)
-    h := crc64.New(table)
-    
-    fd, err := os.Open(s)
-    if (err != nil) {
-        fmt.Printf("error reading file %s\n", s)
-        return
-    }
-    
+func cksum(fd *os.File) hash.Hash32 {
+    table := crc32.MakeTable(crc32.Castagnoli)
+    h := crc32.New(table)
     const NBUF = 512
     var buf [NBUF]byte
     for {
         switch nr, er := fd.Read(buf[:]); true {
-            case nr < 0:
-                fmt.Printf("error reading from %s: %s\n", fd, er.String())
-                return
-            case nr == 0: // EOF
-                fmt.Printf("%d %s\n", h.Sum64(), s)
-                return
-            case nr > 0:
-                _, ew := h.Write(buf[0:nr])
-                if ew != nil {
-                    fmt.Printf("error writing into hash buffer\n")
-                    return
-                }
+        case nr < 0:
+            fmt.Printf("error reading from %s: %s\n", fd, er.String())
+            return nil
+        case nr == 0: // EOF
+            //fmt.Printf("%d %d %s\n", h.Sum32(), h.Size(), fd.Name())
+            return h
+        case nr > 0:
+            _, ew := h.Write(buf[0:nr])
+            if ew != nil {
+                fmt.Printf("error writing into hash buffer\n")
+                return nil
+            }
         }
     }
+    return nil
 }
 
