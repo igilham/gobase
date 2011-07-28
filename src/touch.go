5c0dde809662768f15ad5bb9d775d068e6320a34
Ian Gilham <ian.gilham@gmail.com>
2011-07-28 19:45:40 +0100

    touch can now create files that don't already exist

--- src/touch.go
+++ src/touch.go
@@ -9,14 +9,21 @@ import (
     "time"
 )
 
-// Change file timestamps
+// Change file timestamps or create files that don't already exist
 func main() {
     flag.Parse()
+    if flag.NArg() == 0 {
+        fmt.Printf("touch: missing file operand\n")
+        os.Exit(1)
+    }
     for i := 0; i < flag.NArg(); i++ {
         now := time.Nanoseconds()
         err := os.Chtimes(flag.Arg(i), now, now)
         if err != nil {
-            fmt.Printf("touch: cannot acccess file " + flag.Arg(i))
+            _, ew := os.Create(flag.Arg(i))
+            if ew != nil {
+                fmt.Printf("touch: cannot create file %s\n", flag.Arg(i))
+            }
         }
     }
 }
