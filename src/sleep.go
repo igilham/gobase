acfd8f417d4eb0b6b931977762fa4a993dc232bf
Carlos Jos? Ruiz-Henestrosa Ruiz <ruizh.cj@gmail.com>
2011-10-23 18:13:59 +0200

    Added a full implementation of sleep

--- src/sleep.go
+++ src/sleep.go
@@ -1,34 +1,49 @@
-// sleep.go
-
 package main
 
 import (
-    "flag"
-    "fmt"
-    "os"
-    "time"
-    "strconv"
+	"flag"
+	"fmt"
+	"os"
+	"time"
+	"strconv"
 )
 
-func usage() {
-    fmt.Printf("usage: sleep MILLISECONDS\n")
-}
+var factor float64
+var number int64
+var amountOfTime int64
 
-// BUG: there is an error in here somewhere. It compiles without warnings
-// but does not correctly sleep
 func main() {
-    flag.Parse()
-    if flag.NArg() != 1 {
-        fmt.Printf("sleep: invalid arguments\n")
-        os.Exit(1)
-    }
-    str := flag.Arg(0)
-    sleeptime, er := strconv.Atoi64(str)
-    if er != nil {
-        usage()
-    } else {
-        // BUG: sleep doesn't work
-        time.Sleep(sleeptime)
-    }
-}
+	flag.Parse()
+	if flag.NArg() == 0 {
+		fail()
+	}
+	for i := 0; i < flag.NArg(); i++ {
+		switch flag.Arg(0)[len(flag.Arg(0))-1] {
+		case 'm':
+			factor = 6e10
+			number, _ = strconv.Atoi64(flag.Arg(i)[:len(flag.Arg(i))-1])
+		case 'h':
+			factor = 36e11
+			number, _ = strconv.Atoi64(flag.Arg(i)[:len(flag.Arg(i))-1])
+		case 'd':
+			factor = 864e11
+			number, _ = strconv.Atoi64(flag.Arg(i)[:len(flag.Arg(i))-1])
+		case 's':
+			factor = 1e9
+			number, _ = strconv.Atoi64(flag.Arg(i)[:len(flag.Arg(i))-1])
+		case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+			factor = 1e9
+			number, _ = strconv.Atoi64(flag.Arg(i))
+		default:
+			fail()
 
+		}
+		amountOfTime += number * int64(factor)
+
+	}
+	time.Sleep(amountOfTime)
+}
+func fail() {
+	fmt.Println("sleep: invalid arguments")
+	os.Exit(1)
+}
