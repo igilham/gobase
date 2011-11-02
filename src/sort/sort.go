75d804cddcc4ee6a6b24f9bdc76c8b29ad52bb63
Ian Gilham <ian.gilham@gmail.com>
2011-11-02 21:41:12 +0000

    #22 - added a simple implementation of sort

---
+++ src/sort/sort.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"flag"
+	"fmt"
+	"os"
+	"sort"
+)
+
+var reverse = flag.Bool("r", false, "reverse sort order")
+
+var lines []string
+
+// sort input lines
+func main() {
+	flag.Parse()
+    if flag.NArg() == 0 {
+		getlines(os.Stdin)
+    } else {
+		for i := 0; i < flag.NArg(); i++ {
+			if file, err := os.Open(flag.Arg(i)); err != nil {
+				fmt.Fprintf(os.Stderr,
+					"sort: cannot open file %s\n",
+					flag.Arg(i))
+			} else {
+				getlines(file)
+			}
+		}
+    }
+    if len(lines) > 0 {
+		sort.Strings(lines)
+		out(lines)
+	}
+}
+
+func getlines(file *os.File) {
+	var part []byte
+	var	prefix bool
+	var err os.Error
+	reader := bufio.NewReader(file)
+	buffer := bytes.NewBuffer(make([]byte, 1024))
+	for {
+		if part, prefix, err = reader.ReadLine(); err != nil {
+			break
+		}
+		buffer.Write(part)
+		if !prefix {
+			lines = append(lines, buffer.String())
+			buffer.Reset()
+		}
+	}
+}
+
+func out(list []string) {
+	var (
+		start int64 = 0
+		end int64 = int64(len(list))
+		step int64 = 1
+	)
+	if *reverse {
+		step = -1
+		start = end - 1
+		end = -1
+	}
+	for i := start; i != end; i += step {
+		fmt.Println(list[i])
+	}
+}
