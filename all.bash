64a755821d16f7490b8381303cbaaf1555ec05b9
Ian Gilham <ian.gilham@gmail.com>
2012-05-04 23:55:39 +0100

    added simple windows build file

--- all.bash
+++ all.bash
@@ -1,21 +1,27 @@
-#¬/bin/bash
+#!/bin/bash
 
-go build src/basename/basename.go
-go build src/cat/cat.go
-go build src/cksum/cksum.go
-go build src/dirname/dirname.go
-go build src/echo/echo.go
-go build src/false/false.go
-go build src/head/head.go
-go build src/ls/ls.go
-go build src/mkdir/mkdir.go
-go build src/pwd/pwd.go
-go build src/rm/rm.go
-go build src/sleep/sleep.go
-go build src/sort/sort.go
-go build src/tee/tee.go
-go build src/touch/touch.go
-go build src/true/true.go
-go build src/wc/wc.go
-go build src/whoami/whoami.go
-go build src/yes/yes.go
+OUTDIR="_bin"
+
+if [[ ! -d $OUTDIR ]]; then
+  mkdir $OUTDIR
+fi
+
+go build -o $OUTDIR/basename src/basename/basename.go
+go build -o $OUTDIR/cat src/cat/cat.go
+go build -o $OUTDIR/cksum src/cksum/cksum.go
+go build -o $OUTDIR/dirname src/dirname/dirname.go
+go build -o $OUTDIR/echo src/echo/echo.go
+go build -o $OUTDIR/false src/false/false.go
+go build -o $OUTDIR/head src/head/head.go
+go build -o $OUTDIR/ls src/ls/ls.go
+go build -o $OUTDIR/mkdir src/mkdir/mkdir.go
+go build -o $OUTDIR/pwd src/pwd/pwd.go
+go build -o $OUTDIR/rm src/rm/rm.go
+go build -o $OUTDIR/sleep src/sleep/sleep.go
+go build -o $OUTDIR/sort src/sort/sort.go
+go build -o $OUTDIR/tee src/tee/tee.go
+go build -o $OUTDIR/touch src/touch/touch.go
+go build -o $OUTDIR/true src/true/true.go
+go build -o $OUTDIR/wc src/wc/wc.go
+go build -o $OUTDIR/whoami src/whoami/whoami.go
+go build -o $OUTDIR/yes src/yes/yes.go
