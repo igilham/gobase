dab9d892fe0bdb1a01ec27550c2a3077580b15b3
Ian Gilham <ian.gilham@gmail.com>
2014-06-04 18:21:38 +0100

    consolidated pwd code

--- src/pwd/pwd.go
+++ src/pwd/pwd.go
@@ -1,10 +1,17 @@
 package main
 
 import (
-	"gobase"
+	"fmt"
+	"log"
+	"os"
 )
 
 // prints the current working directory
 func main() {
-	gobase.Pwd()
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Fatalf("pwd: %v", err)
+	} else {
+		fmt.Println(wd)
+	}
 }
